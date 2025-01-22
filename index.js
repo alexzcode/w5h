a518ae5cfaa3f48af1e34e6038ef42f2eb705dbe
alexzcode <121963160+alexzcode@users.noreply.github.com>
2025-01-22 15:20:54 -0500

    test 139649813

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 let qno = 1;
 let waiting = false;
+let correctno = 0
+let as = [];
 var q = document.getElementById("q")
 var a = document.getElementById("a")
 var btn1 = document.getElementById("a1")
 var btn2 = document.getElementById("a2")
 var btn3 = document.getElementById("a3")
 var btn4 = document.getElementById("a4")
-function question(qs,a11,a22,a33,a44) {
+function shuffle(array) {
+  let currentIndex = array.length;
+  while (currentIndex != 0) {
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+}
+function question(qs,aw,ac) {
+  as.push(aw)
+  ac.push(ac)
   q.innerText = "Question "+String(qno)+": "+String(qs)
   a.innerText = "Waiting on answer..."
-  btn1.innerText = a11
-  btn2.innerText = a22
-  btn3.innerText = a33
-  btn4.innerText = a44
+  btn1.innerText = as[0]
+  btn2.innerText = as[1]
+  btn3.innerText = as[2]
+  btn4.innerText = as[3]
 }
 fetch("https://opentdb.com/api.php?amount=1")
   .then((response) => response.json())
-  .then((json) => question(json.results[0].question,"a 1","a 2","a 3","a 4"));
+  .then((json) => question(json.results[0].question,json.results[0].incorrect_answers,json.results[0].correct_answers));
 function sleep(s) {
   return new Promise((resolve) => setTimeout(resolve, s * 1000));
+}
+function one() {
+  selected = 1;
+}
+function two() {
+  selected = 2;
+}
+function three() {
+  selected = 3;
+}
+function four() {
+  selected = 4;
 }
