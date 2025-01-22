b06d0a412936c64a023d069d376ff91c257f5855
alexzcode <121963160+alexzcode@users.noreply.github.com>
2025-01-22 14:45:29 -0500

    test 32,461

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var usrinput = document.getElementById("usrinput");
-usrinput.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    console.log("Enter key pressed!");
-    fetch("https://opentdb.com/api.php?amount=1")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  }
-});
+let qno = 1;
+let waiting = false;
+var q = document.getElementById("q")
+var a = document.getElementById("a")
+var btn1 = document.getElementById("a1")
+var btn2 = document.getElementById("a2")
+var btn3 = document.getElementById("a3")
+var btn4 = document.getElementById("a4")
+function question(qs,a11,a22,a33,a44) {
+  q.innerText = "Question "+String(qno)+": "+String(qs)
+  a.innerText = "Waiting on answer..."
+  btn1.innerText = a11
+  btn2.innerText = a22
+  btn3.innerText = a33
+  btn4.innerText = a44
+}
+fetch("https://opentdb.com/api.php?amount=1")
+  .then((response) => response.json())
+  .then((json) => question(json.results[0].question,"a 1","a 2","a 3","a 4"));
+function sleep(s) {
+  return new Promise((resolve) => setTimeout(resolve, s * 1000));
+}
